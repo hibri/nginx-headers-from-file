c1c22a2b4e00ee598027cd55bf563dd55d3e77dc
Hibri Marzook <hibri@hibri.net>
2013-01-25 07:58:15 +0000

    write custom headers only. leave other output as is

--- ngx_http_hello_world_filter.c
+++ ngx_http_hello_world_filter.c
@@ -18,11 +18,11 @@ static ngx_command_t  ngx_http_hello_world_commands[] = {
 };
 
 
-static u_char  ngx_hello_world[] = "hello world";
+//static u_char  ngx_hello_world[] = "hello world";
 
 static ngx_http_module_t  ngx_http_hello_world_module_ctx = {
   NULL,                          /* preconfiguration */
-  NULL,                          /* postconfiguration */
+  ngx_http_hello_world_filter_init,                          /* postconfiguration */
 
   NULL,                          /* create main configuration */
   NULL,                          /* init main configuration */
@@ -50,30 +50,43 @@ ngx_module_t ngx_http_hello_world_filter = {
   NGX_MODULE_V1_PADDING
 };
 
+static ngx_http_output_header_filter_pt ngx_http_next_header_filter;
+static ngx_http_output_body_filter_pt   ngx_http_next_body_filter;
+ngx_table_elt_t *my_custom_header;
 
 static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t *r)
 {
-  ngx_buf_t    *b;
-  ngx_chain_t   out;
+  
 
   r->headers_out.content_type.len = sizeof("text/funky") - 1;
   r->headers_out.content_type.data = (u_char *) "text/funky";
-
-  b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
-
-  out.buf = b;
-  out.next = NULL;
-
-  b->pos = ngx_hello_world;
-  b->last = ngx_hello_world + sizeof(ngx_hello_world);
-  b->memory = 1;
-  b->last_buf = 1;
-
-  r->headers_out.status = NGX_HTTP_OK;
   
-  ngx_http_send_header(r);
+  //
+  ngx_table_elt_t  *h;
 
-  return ngx_http_output_filter(r, &out);
+   
+        h = ngx_list_push(&r->headers_out.headers);
+        if (h == NULL) {
+            return NGX_ERROR;
+        }
+
+        h->hash = 1;
+        ngx_str_set(&h->key, "X-Hibri");
+
+        
+        h->value.len = sizeof("My Header Value") -1;
+        h->value.data = (u_char *) "My Header Value";
+
+    
+
+  //
+  
+//ngx_http_clear_content_length(r);
+
+
+/* step 3: call the next filter */
+
+  return ngx_http_next_header_filter(r);
 }
 
 
@@ -82,7 +95,18 @@ static char *ngx_http_hello_world(ngx_conf_t *cf, ngx_command_t *cmd, void *conf
   ngx_http_core_loc_conf_t  *clcf;
 
   clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-  clcf->handler = ngx_http_hello_world_handler;
+  
 
   return NGX_CONF_OK;
 }
+
+static ngx_int_t ngx_http_hello_world_filter_init(ngx_conf_t *cf)
+{
+    ngx_http_next_header_filter = ngx_http_top_header_filter;
+    ngx_http_top_header_filter = ngx_http_hello_world_handler;
+
+    ngx_http_next_body_filter = ngx_http_top_body_filter;
+    ngx_http_top_body_filter = ngx_http_top_body_filter;
+    return NGX_OK;
+}
+
